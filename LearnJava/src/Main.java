2e97bdfddc641499465ee43da5aaf879b5695779
Malcolm <macscott245@outlook.com>
2021-09-25 01:00:04 -0600

    Use the scanner class

--- LearnJava/src/Main.java
+++ LearnJava/src/Main.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Please enter your name: ");
+		String name = scanner.nextLine();
+		
+		System.out.println("Please enter your last name: ");
+		String lastName = scanner.nextLine();
+		
+		System.out.println("Please enter how many siblings you have: ");
+		int size = scanner.nextInt();
+		scanner.nextLine();
+		String[] eachSibling = new String[size];
+		System.out.println("Enter one siblings first name, press enter than enter the next: ");
+		
+		for(int i = 0; i < size; i++){
+			
+			eachSibling[i] = scanner.nextLine();
+		}
+		
+		System.out.println("Hello " + name + " " + lastName);
+		
+		for(int i = 0; i < size; i++) {
+			System.out.println("Hello " + eachSibling[i] + " " + lastName);
+		}
 	}
 
 }
