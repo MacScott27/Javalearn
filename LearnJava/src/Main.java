915a4205867c5344b97cbe71f2dc483345cd39ea
Malcolm <macscott245@outlook.com>
2021-09-25 12:45:48 -0600

    add stacks example

--- LearnJava/src/Main.java
+++ LearnJava/src/Main.java
@@ -1,32 +1,15 @@
-import java.util.Scanner;
+import java.util.LinkedList;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		LinkedList<String> names = new LinkedList<String>();
+		names.push("Caleb");
+		names.push("Sue");
+		names.push("Sally");
 		
-		System.out.println("Please enter your name: ");
-		String name = scanner.nextLine();
-		
-		System.out.println("Please enter your last name: ");
-		String lastName = scanner.nextLine();
-		
-		System.out.println("Please enter how many siblings you have: ");
-		int size = scanner.nextInt();
-		scanner.nextLine();
-		String[] eachSibling = new String[size];
-		System.out.println("Enter one siblings first name, press enter than enter the next: ");
-		
-		for(int i = 0; i < size; i++){
-			
-			eachSibling[i] = scanner.nextLine();
-		}
-		
-		System.out.println("Hello " + name + " " + lastName);
-		
-		for(int i = 0; i < size; i++) {
-			System.out.println("Hello " + eachSibling[i] + " " + lastName);
-		}
+		System.out.println(names.pop());
+		System.out.println(names.pop());
+		System.out.println(names.pop());
 	}
-
 }
